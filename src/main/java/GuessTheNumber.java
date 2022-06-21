738fa6972e8f469067e1bf0888e3ad43abfc71c0
johnoro <officialjohnorourke@gmail.com>
2022-06-21 11:13:10 -0400

    Completes base project and some edge case handling.

---
+++ src/main/java/GuessTheNumber.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Hello! What is your name?");
+        String name = s.nextLine();
+
+        while (true) {
+            System.out.println("Well, " + name + ", I'm thinking of a number between 1 and 20.");
+
+            int guess = -999; // default number for first loop
+            int numGuesses = 0;
+            // rand decimal * range + min #
+            int number = (int) (Math.random() * 20) + 1;
+
+            do {
+                if (guess != -999) {
+                    if (guess > 20 || guess < 1) {
+                        System.out.println("Remember, the number is between 1 and 20!");
+                    } else {
+                        if (guess > number) System.out.println("Your guess is too high.");
+                        else System.out.println("Your guess is too low.");
+                    }
+                }
+
+                System.out.println("Take a guess.");
+                guess = s.nextInt();
+                s.nextLine(); // clears out newline for playAnswer y/n
+                numGuesses++;
+            } while (guess != number);
+
+            System.out.println("Good job, " + name + "! You guessed my number in " + numGuesses + " guesses!");
+            System.out.println("Would you like to play again? (y or n)");
+
+            String playAnswer = s.nextLine();
+
+            if (playAnswer.compareTo("n") == 0) break;
+            if (playAnswer.compareTo("y") != 0) {
+                System.out.println("A \"y\" or \"n\" wasn't detected.");
+                System.out.println("I'll just assume you meant yes. for simplicity's sake.");
+            }
+
+        }
+    }
+}
